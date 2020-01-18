04346dfc6881410b8c9bb86e73717d9a57a214eb
MariosRichards <Marios.Richards@gmail.com>
2020-01-18 18:12:15 +0100

    Political Compass Dominance Plot updated with holoviews

--- utility.py
+++ utility.py
@@ -75,6 +75,7 @@ valid_filename_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
 
 
 def clean_filename(filename, whitelist=valid_filename_chars, replace=' ', char_limit = 30):
+    import warnings
     # replace spaces
     for r in replace:
         filename = filename.replace(r,'_')
@@ -85,7 +86,9 @@ def clean_filename(filename, whitelist=valid_filename_chars, replace=' ', char_l
     # keep only whitelisted chars
     cleaned_filename = ''.join(c for c in cleaned_filename if c in whitelist)
     if len(cleaned_filename)>char_limit:
-        print("Warning, filename truncated because it was over {}. Filenames may no longer be unique".format(char_limit))
+
+        warnings.warn("Warning, filename truncated because it was over {}. Filenames may no longer be unique".format(char_limit))
+        # print("Warning, filename truncated because it was over {}. Filenames may no longer be unique".format(char_limit))
     return cleaned_filename[:char_limit]    
 
 def create_subdir(base_dir, subdir, char_limit=50):
